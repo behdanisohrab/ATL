eddcf9df2960aab1a25ed11152e3652ce9302640
Mis012 <michael.srba@seznam.cz>
2021-10-28 13:28:46 +0200

    remove the no longer needed apk-specific import

--- main-src/org/launch/main.java
+++ main-src/org/launch/main.java
@@ -4,8 +4,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.demo_application.MainActivity; // FIXME - get this dynamically (at least take it as a cmdline parameter)
-
 public class main {
 	public static void main(String[] args) {
 		System.load("/home/Mis012/Github_and_other_sources/android_translation_layer_PoC/libnative/org_launch_main.so");
