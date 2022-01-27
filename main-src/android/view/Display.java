43f21dcbc15700f851c2a83916581601b1bab39a
Mis012 <michael.srba@seznam.cz>
2022-01-27 21:57:47 +0100

    FIXME: put better defaults for 'getDefaultDisplay().getMetrics' than width/height of 0

--- main-src/android/view/Display.java
+++ main-src/android/view/Display.java
@@ -3,7 +3,12 @@ package android.view;
 import android.util.DisplayMetrics;
 
 public final class Display {
-	public void getMetrics(DisplayMetrics outMetrics) {}
+	// FIXME: what do we return here?
+	// we don't want to hardcode this stuff, but at the same time the apps can cache it
+	public void getMetrics(DisplayMetrics outMetrics) {
+		outMetrics.widthPixels = 960;
+		outMetrics.heightPixels = 540;
+	}
 
 	public int getRotation() {
 		return 0/*ROTATION_0*/;
