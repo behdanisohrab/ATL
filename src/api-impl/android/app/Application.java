6835702c8ecc3af9620520c684c2c7663c3261c8
Nikita Travkin <nikita@trvn.ru>
2022-11-04 21:18:44 +0500

    fixme-desc: add some stubs

--- src/api-impl/android/app/Application.java
+++ src/api-impl/android/app/Application.java
@@ -1,11 +1,70 @@
 package android.app;
+import android.os.Bundle;
+import android.content.res.Configuration;
 
 import android.content.Context;
 
 public class Application extends Context {
-	public interface ActivityLifecycleCallbacks {}
-
-	public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
-
+	public interface ActivityLifecycleCallbacks {
+		void onActivityCreated(Activity activity, Bundle savedInstanceState);
+		void onActivityStarted(Activity activity);
+		void onActivityResumed(Activity activity);
+		void onActivityPaused(Activity activity);
+		void onActivityStopped(Activity activity);
+		void onActivitySaveInstanceState(Activity activity, Bundle outState);
+		void onActivityDestroyed(Activity activity);
+	}
+	/**
+	 * Callback interface for use with {@link Application#registerOnProvideAssistDataListener}
+	 * and {@link Application#unregisterOnProvideAssistDataListener}.
+	 */
+	public interface OnProvideAssistDataListener {
+		/**
+		 * This is called when the user is requesting an assist, to build a full
+		 * {@link Intent#ACTION_ASSIST} Intent with all of the context of the current
+		 * application.  You can override this method to place into the bundle anything
+		 * you would like to appear in the {@link Intent#EXTRA_ASSIST_CONTEXT} part
+		 * of the assist Intent.
+		 */
+		public void onProvideAssistData(Activity activity, Bundle data);
+	}
+	public Application() {
+	}
+	/**
+	 * Called when the application is starting, before any activity, service,
+	 * or receiver objects (excluding content providers) have been created.
+	 * Implementations should be as quick as possible (for example using
+	 * lazy initialization of state) since the time spent in this function
+	 * directly impacts the performance of starting the first activity,
+	 * service, or receiver in a process.
+	 * If you override this method, be sure to call super.onCreate().
+	 */
+	public void onCreate() {
+	}
+	/**
+	 * This method is for use in emulated process environments.  It will
+	 * never be called on a production Android device, where processes are
+	 * removed by simply killing them; no user code (including this callback)
+	 * is executed when doing so.
+	 */
+	public void onTerminate() {
+	}
+	public void onConfigurationChanged(Configuration newConfig) {
+	}
+	public void onLowMemory() {
+	}
+	public void onTrimMemory(int level) {
+	}
+/*	public void registerComponentCallbacks(ComponentCallbacks callback) {
+	}
+	public void unregisterComponentCallbacks(ComponentCallbacks callback) {
+	}*/
+	public void registerActivityLifecycleCallbacks(ActivityLifecycleCallbacks callback) {
+	}
+	public void unregisterActivityLifecycleCallbacks(ActivityLifecycleCallbacks callback) {
+	}
+	public void registerOnProvideAssistDataListener(OnProvideAssistDataListener callback) {
+	}
+	public void unregisterOnProvideAssistDataListener(OnProvideAssistDataListener callback) {
 	}
 }
---
+++ src/api-impl/android/os/Debug.java
@@ -0,0 +1,15 @@
+package android.os;
+public final class Debug
+{
+    public static class MemoryInfo {
+    }
+    /**
+     * Wait until a debugger attaches.  As soon as the debugger attaches,
+     * this returns, so you will need to place a breakpoint after the
+     * waitForDebugger() call if you want to start tracing immediately.
+     */
+    public static class InstructionCount {
+        public InstructionCount() {
+        }
+    }
+}
