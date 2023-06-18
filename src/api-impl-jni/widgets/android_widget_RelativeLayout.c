1a8e198e05b6d36cc13858665d4436db4e6b63ed
Julian Winkler <julian.winkler1@web.de>
2023-06-18 11:22:23 +0200

    make RelativeLayout vertical by default and vexpand GLSurfaceView

--- src/api-impl-jni/widgets/android_widget_RelativeLayout.c
+++ src/api-impl-jni/widgets/android_widget_RelativeLayout.c
@@ -12,7 +12,7 @@
 
 JNIEXPORT void JNICALL Java_android_widget_RelativeLayout_native_1constructor__Landroid_util_AttributeSet_2(JNIEnv *env, jobject this, jobject attrs)
 {
-	int orientation = attribute_set_get_int(env, attrs, "orientation", NULL, 0);
+	int orientation = attribute_set_get_int(env, attrs, "orientation", NULL, 1);
 
 	GtkWidget *wrapper = wrapper_widget_new();
 	GtkWidget *box = gtk_box_new(orientation ? GTK_ORIENTATION_VERTICAL : GTK_ORIENTATION_HORIZONTAL, 1); // spacing of 1
@@ -23,7 +23,7 @@ JNIEXPORT void JNICALL Java_android_widget_RelativeLayout_native_1constructor__L
 
 JNIEXPORT void JNICALL Java_android_widget_RelativeLayout_native_1constructor__Landroid_content_Context_2(JNIEnv *env, jobject this, jobject context) {
 	GtkWidget *wrapper = wrapper_widget_new();
-	GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 1); // spacing of 1
+	GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 1); // spacing of 1
 	wrapper_widget_set_child(WRAPPER_WIDGET(wrapper), box);
 	gtk_widget_set_name(GTK_WIDGET(box), "RelativeLayout");
 	_SET_LONG_FIELD(this, "widget", _INTPTR(box));
