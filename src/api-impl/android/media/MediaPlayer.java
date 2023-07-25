6a39ad031f504a0d8ef9b0da0dfae1c9873152e6
Mis012 <Michael.Srba@seznam.cz>
2023-07-26 00:46:38 +0200

    api-impl/android/media/MediaPlayer.java: add more stubs
    this fixes a regression with Cut The Rope, which previously didn't
    find the lack of these fatal

--- src/api-impl/android/media/MediaPlayer.java
+++ src/api-impl/android/media/MediaPlayer.java
@@ -1,6 +1,7 @@
 package android.media;
 
 import android.content.Context;
+import java.io.FileDescriptor;
 
 public class MediaPlayer {
 	public interface OnCompletionListener {
@@ -21,4 +22,18 @@ public class MediaPlayer {
 	}
 
 	public static MediaPlayer create(Context context, int dummy) { return new MediaPlayer(); }
+
+	public void setDataSource(FileDescriptor src, long dummy, long dummy2) {}
+	public void setLooping(boolean dummy) {}
+	public void setOnCompletionListener(MediaPlayer.OnCompletionListener dummy) {}
+	public void setOnErrorListener(MediaPlayer.OnErrorListener dummy) {}
+	public void setOnPreparedListener(MediaPlayer.OnPreparedListener dummy) {}
+	public void setOnBufferingUpdateListener(MediaPlayer.OnBufferingUpdateListener dummy) {}
+	public void setOnInfoListener(MediaPlayer.OnInfoListener dummy) {}
+	public void setOnSeekCompleteListener(MediaPlayer.OnSeekCompleteListener dummy) {}
+	public void setOnVideoSizeChangedListener(MediaPlayer.OnVideoSizeChangedListener dummy) {}
+	public void setAudioStreamType(int dummy) {}
+	public void prepareAsync() {}
+	public void reset() {}
+	public void release() {}
 }
