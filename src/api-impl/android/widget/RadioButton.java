c6c4e8b3a2f634836a5a4411fade1356652e3b0e
Julian Winkler <julian.winkler1@web.de>
2023-08-06 14:10:48 +0200

    add stub RadioButton class

---
+++ src/api-impl/android/widget/RadioButton.java
@@ -0,0 +1,17 @@
+package android.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class RadioButton extends View {
+
+	public RadioButton(Context context) {
+		super(context);
+	}
+
+	public RadioButton(Context context, AttributeSet attributeSet) {
+		super(context, attributeSet);
+	}
+
+}
