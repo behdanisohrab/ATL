faf4a3281e26a518d3edeea091582b6d0aa6f29b
Julian Winkler <julian.winkler1@web.de>
2023-08-22 13:29:44 +0200

    add initial support for fragments
    Fragments are not added to the View hierarchy yet. Only the lifecycle
    callbacks are implemented

--- src/api-impl/android/app/Fragment.java
+++ src/api-impl/android/app/Fragment.java
@@ -1,5 +1,31 @@
 package android.app;
 
+import android.os.Bundle;
+
 public class Fragment {
-	
+
+	Activity activity;
+
+	public void onCreate(Bundle savedInstanceState) {
+	}
+
+	public void onStart() {
+	}
+
+	public void onResume() {
+	}
+
+	public void onPause() {
+	}
+
+	public void onStop() {
+	}
+
+	public void onDestroy() {
+	}
+
+	public Activity getActivity() {
+		return activity;
+	}
+
 }
--- src/api-impl/android/app/FragmentTransaction.java
+++ src/api-impl/android/app/FragmentTransaction.java
@@ -1,8 +1,16 @@
 package android.app;
 
 public class FragmentTransaction {
+
+	private Activity activity;
+
+	public FragmentTransaction(Activity activity) {
+		this.activity = activity;
+	}
 	
 	public FragmentTransaction add(Fragment fragment, String string) {
+		fragment.activity = activity;
+		activity.fragments.add(fragment);
 		return this;
 	}
 
