d370ec80de8d0251c793028650e40c568f3e3ba8
Julian Winkler <julian.winkler1@web.de>
2023-08-22 13:55:56 +0200

    implement android.widget.EditText using GtkEntry

---
+++ src/api-impl/android/text/Editable.java
@@ -0,0 +1,5 @@
+package android.text;
+
+public interface Editable extends CharSequence {
+
+}
--- src/api-impl/android/widget/EditText.java
+++ src/api-impl/android/widget/EditText.java
@@ -1,8 +1,8 @@
 package android.widget;
 
 import android.content.Context;
+import android.text.Editable;
 import android.util.AttributeSet;
-import android.view.View;
 
 public class EditText extends TextView {
 	public EditText(Context context) {
@@ -11,5 +11,51 @@ public class EditText extends TextView {
 
 	public EditText(Context context, AttributeSet attrs) {
 		super(context, attrs);
+	}
+
+	@Override
+	protected native long native_constructor(Context context, AttributeSet attrs);
+	protected native String native_getText(long widget);
+
+	public Editable getText() {
+		return new FIXME_Editable(native_getText(widget));
+	}
+
+	public Editable getEditableText() {
+		return new FIXME_Editable(native_getText(widget));
+	}
+
+	@Override
+	public void setText(CharSequence text) {}
+	@Override
+	public void setTextSize(float size) {}
+
+	private class FIXME_Editable implements Editable {
+		private String string;
+
+		public FIXME_Editable(String string) {
+			this.string = string;
+		}
+
+		@Override
+		public int length() {
+			return string.length();
+		}
+
+		@Override
+		public char charAt(int index) {
+			return string.charAt(index);
+		}
+
+		@Override
+		public CharSequence subSequence(int start, int end) {
+			return string.subSequence(start, end);
+		}
+
+		@Override
+		public String toString() {
+			return string;
+		}
+
 	}
 }
