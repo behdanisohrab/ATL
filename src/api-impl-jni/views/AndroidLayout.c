0714ef02b23efc9be9ce74d4bb0b59962622eda7
Julian Winkler <julian.winkler1@web.de>
2023-10-31 22:56:22 +0100

    android_layout_measure(): refactor to respect LayoutParams
    This is needed when a Java widget gets measured from GTK

--- src/api-impl-jni/views/AndroidLayout.c
+++ src/api-impl-jni/views/AndroidLayout.c
@@ -3,10 +3,35 @@
 #include "../util.h"
 #include "AndroidLayout.h"
 
+static int make_measure_spec(int layout_size, int for_size)
+{
+	if (layout_size >= 0)
+		return layout_size | MEASURE_SPEC_EXACTLY;
+	else if (for_size >= 0 && layout_size == MATCH_PARENT)
+		return for_size | MEASURE_SPEC_EXACTLY;
+	else if (for_size >= 0 && layout_size == WRAP_CONTENT)
+		return for_size | MEASURE_SPEC_AT_MOST;
+	else if (layout_size == WRAP_CONTENT)
+		return MEASURE_SPEC_UNSPECIFIED;
+	else
+		return -1;
+}
+
 static void android_layout_measure(GtkLayoutManager *layout_manager, GtkWidget *widget, GtkOrientation orientation, int for_size, int *minimum, int *natural, int *minimum_baseline, int *natural_baseline)
 {
 	AndroidLayout *layout = ATL_ANDROID_LAYOUT(layout_manager);
 	JNIEnv *env = get_jni_env();
+
+	if (layout->width || layout->height) {
+		int widthMeasureSpec = make_measure_spec(layout->width, orientation == GTK_ORIENTATION_VERTICAL ? for_size : -1);
+		int heightMeasureSpec = make_measure_spec(layout->height, orientation == GTK_ORIENTATION_HORIZONTAL ? for_size : -1);
+
+		if (widthMeasureSpec != -1 && heightMeasureSpec != -1) {
+			(*env)->CallVoidMethod(env, layout->view, handle_cache.view.measure, widthMeasureSpec, heightMeasureSpec);
+			if((*env)->ExceptionCheck(env))
+				(*env)->ExceptionDescribe(env);
+		}
+	}
 
 	if (orientation == GTK_ORIENTATION_HORIZONTAL) {
 		*minimum = (*env)->CallIntMethod(env, layout->view, handle_cache.view.getSuggestedMinimumWidth);
@@ -48,4 +73,10 @@ GtkLayoutManager *android_layout_new(jobject view)
 	AndroidLayout *layout = g_object_new(android_layout_get_type(), NULL);
 	layout->view = view;
 	return &layout->parent_instance;
+}
+
+void android_layout_set_params(AndroidLayout *layout, int width, int height)
+{
+	layout->width = width;
+	layout->height = height;
 }
--- src/api-impl-jni/views/AndroidLayout.h
+++ src/api-impl-jni/views/AndroidLayout.h
@@ -4,13 +4,24 @@
 #include <jni.h>
 #include <gtk/gtk.h>
 
+#define MEASURE_SPEC_UNSPECIFIED (0 << 30)
+#define MEASURE_SPEC_EXACTLY (1 << 30)
+#define MEASURE_SPEC_AT_MOST (2 << 30)
+#define MEASURE_SPEC_MASK (0x3 << 30)
+
+#define MATCH_PARENT (-1)
+#define WRAP_CONTENT (-2)
+
 G_DECLARE_FINAL_TYPE(AndroidLayout, android_layout, ATL, ANDROID_LAYOUT, GtkLayoutManager);
 
 struct _AndroidLayout {
 	GtkLayoutManager parent_instance;
 	jobject view;
+	int width;
+	int height;
 };
 
 GtkLayoutManager *android_layout_new(jobject view);
+void android_layout_set_params(AndroidLayout *layout, int width, int height);
 
 #endif  // ANDROID_LAYOUT_H
