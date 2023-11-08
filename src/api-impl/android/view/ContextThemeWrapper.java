9f74ab811eee1d688a12b0a9a529b2b9fcef7de0
Julian Winkler <julian.winkler1@web.de>
2023-11-08 21:29:46 +0100

    LayoutInflater: support android:theme attribute

--- src/api-impl/android/view/ContextThemeWrapper.java
+++ src/api-impl/android/view/ContextThemeWrapper.java
@@ -2,11 +2,26 @@ package android.view;
 
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.res.Resources;
 
 public class ContextThemeWrapper extends ContextWrapper {
 
+	private Resources.Theme theme = getResources().newTheme();
+
 	public ContextThemeWrapper(Context context, int themeResId) {
 		super(context);
+		theme.setTo(context.getTheme());
+		setTheme(themeResId);
+	}
+
+	@Override
+	public void setTheme(int resid) {
+		theme.applyStyle(resid, true);
+	}
+
+	@Override
+	public Resources.Theme getTheme() {
+		return theme;
 	}
 
 }
