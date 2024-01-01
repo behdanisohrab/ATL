c5b315cb3812207ee4eef552030d27d0e729df91
Julian Winkler <julian.winkler1@web.de>
2024-01-01 12:10:45 +0100

    LinearLayout: remove 1px spacing

--- src/api-impl-jni/widgets/android_widget_LinearLayout.c
+++ src/api-impl-jni/widgets/android_widget_LinearLayout.c
@@ -22,7 +22,7 @@ JNIEXPORT jlong JNICALL Java_android_widget_LinearLayout_native_1constructor(JNI
 	int orientation = attribute_set_get_int(env, attrs, "orientation", NULL, 0);
 
 	GtkWidget *wrapper = g_object_ref(wrapper_widget_new());
-	GtkWidget *box = gtk_box_new(orientation ? GTK_ORIENTATION_VERTICAL : GTK_ORIENTATION_HORIZONTAL, 1); // spacing of 1
+	GtkWidget *box = gtk_box_new(orientation ? GTK_ORIENTATION_VERTICAL : GTK_ORIENTATION_HORIZONTAL, 0); // spacing of 0
 	wrapper_widget_set_child(WRAPPER_WIDGET(wrapper), box);
 	gtk_widget_set_name(GTK_WIDGET(box), "LinearLayout");
 	if (!attrs) {
