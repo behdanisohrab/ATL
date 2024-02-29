60714ebf45cb5ee8057bd605a6cb1f4c51996dbf
Mis012 <Michael.Srba@seznam.cz>
2024-02-29 12:33:48 +0100

    add PathInterpolator stub

---
+++ src/api-impl/android/view/animation/BaseInterpolator.java
@@ -0,0 +1,5 @@
+package android.view.animation;
+
+public abstract class BaseInterpolator implements Interpolator {
+
+}
---
+++ src/api-impl/android/view/animation/PathInterpolator.java
@@ -0,0 +1,13 @@
+package android.view.animation;
+
+public class PathInterpolator extends BaseInterpolator {
+
+	public PathInterpolator(float f1, float f2, float f3, float f4) {
+		super();
+	}
+
+	@Override
+	public float getInterpolation(float input) {
+		return input;
+	}
+}
