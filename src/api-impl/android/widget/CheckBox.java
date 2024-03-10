d22d083eb8119896668a9ce8968e5d7ec1bab4ec
Julian Winkler <julian.winkler1@web.de>
2024-03-10 16:05:31 +0100

    implement CheckBox and RadioButton using GtkCheckButton

--- src/api-impl/android/widget/CheckBox.java
+++ src/api-impl/android/widget/CheckBox.java
@@ -15,4 +15,27 @@ public class CheckBox extends CompoundButton {
 
 	public void setLines(int lines) {}
 
+	@Override
+	protected native long native_constructor(Context context, AttributeSet attrs);
+
+	@Override
+	public native void setOnCheckedChangeListener(OnCheckedChangeListener listener);
+
+	@Override
+	public native void setChecked(boolean checked);
+
+	@Override
+	public native boolean isChecked();
+
+	@Override
+	public native void setText(CharSequence text);
+
+	// following methods are overridden to prevent calling incompatible methods from superclasses
+	@Override
+	public void setOnClickListener(final OnClickListener l) {}
+	@Override
+	public void setTextColor(int color) {}
+	@Override
+	public void setTextSize(float size) {}
+
 }
--- src/api-impl/android/widget/RadioButton.java
+++ src/api-impl/android/widget/RadioButton.java
@@ -13,4 +13,27 @@ public class RadioButton extends CompoundButton {
 		super(context, attributeSet);
 	}
 
+	@Override
+	protected native long native_constructor(Context context, AttributeSet attrs);
+
+	@Override
+	public native void setOnCheckedChangeListener(OnCheckedChangeListener listener);
+
+	@Override
+	public native void setChecked(boolean checked);
+
+	@Override
+	public native boolean isChecked();
+
+	@Override
+	public native void setText(CharSequence text);
+
+	// following methods are overridden to prevent calling incompatible methods from superclasses
+	@Override
+	public void setOnClickListener(final OnClickListener l) {}
+	@Override
+	public void setTextColor(int color) {}
+	@Override
+	public void setTextSize(float size) {}
+
 }
