ad4de8edc4df2758d27f51d0fd8e17a0c9109353
Markuss Broks <markuss.broks@gmail.com>
2024-03-16 17:10:38 +0000

    ActivityManager: add a stub for MemoryInfo

--- src/api-impl/android/app/ActivityManager.java
+++ src/api-impl/android/app/ActivityManager.java
@@ -12,4 +12,13 @@ public class ActivityManager {
 
 	public boolean isLowRamDevice() {return false;}
 
+	public static class MemoryInfo {
+		/* For now, just always report there's 10GB free RAM */
+		public long availMem = 10000;
+	}
+
+	public void getMemoryInfo (MemoryInfo outInfo)
+	{
+		outInfo = new MemoryInfo();
+	}
 }
