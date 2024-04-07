677ff43a204c988970bf5d4dad1d715ed34ba39f
Mis012 <Michael.Srba@seznam.cz>
2024-04-07 21:44:37 +0200

    ColorDrawable: add a draw method
    we don't use this directly, but it can be called by an app

--- src/api-impl/android/graphics/drawable/ColorDrawable.java
+++ src/api-impl/android/graphics/drawable/ColorDrawable.java
@@ -1,15 +1,25 @@
 package android.graphics.drawable;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 public class ColorDrawable extends Drawable {
 
 	private int color;
+	private Paint paint;
 
 	public ColorDrawable(int color) {
 		this.color = color;
+		this.paint = new Paint();
+		this.paint.setColor(color);
 	}
 
 	public int getColor() {
 		return color;
 	}
 
+	@Override
+	public void draw(Canvas canvas) {
+		canvas.drawRect(getBounds(), paint);
+	}
 }
